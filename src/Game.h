f94411b7884911c3b429918b483beb1cd65e3e28
Zbigniew Romanowski <romz@wp.pl>
2020-03-07 19:37:20 +0100

    Replace tabulator by four spaces

--- src/Game.h
+++ src/Game.h
@@ -1,11 +1,3 @@
-// ----------------------------------------------------------------
-// From Game Programming in C++ by Sanjay Madhav
-// Copyright (C) 2017 Sanjay Madhav. All rights reserved.
-// 
-// Released under the BSD License
-// See LICENSE in root directory for full details.
-// ----------------------------------------------------------------
-
 #pragma once
 
 #include "SDL.h"
@@ -15,36 +7,36 @@
 class Game
 {
 public:
-	Game();
-	// Initialize the game
-	bool Initialize();
-	// Runs the game loop until the game is over
-	void RunLoop();
-	// Shutdown the game
-	void Shutdown();
+    Game();
+    // Initialize the game
+    bool Initialize();
+    // Runs the game loop until the game is over
+    void RunLoop();
+    // Shutdown the game
+    void Shutdown();
 private:
-	// Helper functions for the game loop
-	void ProcessInput();
-	void UpdateGame();
-	void GenerateOutput();
+    // Helper functions for the game loop
+    void ProcessInput();
+    void UpdateGame();
+    void GenerateOutput();
 
-	// Window created by SDL
-	SDL_Window* mWindow;
-	// Renderer for 2D drawing
-	SDL_Renderer* mRenderer;
-	// Number of ticks since start of game
-	Uint32 mTicksCount;
-	// Game should continue to run
-	bool mIsRunning;
-	
-	// Pong specific
-	// Direction of paddle
-	int mPaddleDir;
-	// Position of paddle
+    // Window created by SDL
+    SDL_Window* mWindow;
+    // Renderer for 2D drawing
+    SDL_Renderer* mRenderer;
+    // Number of ticks since start of game
+    Uint32 mTicksCount;
+    // Game should continue to run
+    bool mIsRunning;
+
+    // Pong specific
+    // Direction of paddle
+    int mPaddleDir;
+    // Position of paddle
     Vector2d mPaddlePos;
-	// Position of ball
+    // Position of ball
     Vector2d mBallPos;
-	// Velocity of ball
+    // Velocity of ball
     Vector2d mBallVel;
 };
 
